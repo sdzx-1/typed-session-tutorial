e9735fb28e6e9414a3a4658c13ca4fb54e532c54
sdzx-1 <shangdizhixia1993@163.com>
2024-09-17 01:55:10 +0800

    cc

--- src/Protocol.hs
+++ src/Protocol.hs
@@ -7,81 +7,111 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE QualifiedDo #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
-{-# OPTIONS_GHC -ddump-splices #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Protocol where
 
-import Data.IFunctor (At, returnAt)
-import qualified Data.IFunctor
-import qualified Data.IFunctor as I
-import GHC.Exts (DataToTag (..), Int (..))
-import TypedSession.Core
+import Data.Kind (Type)
 
+---------------- Mcbirde Indexed Monad--------------------
+infixr 0 ~>
 
-data PingPongRole = Client | Server
+type f ~> g = forall x. f x -> g x
+
+class IFunctor f where
+  imap :: (a ~> b) -> f a ~> f b
+
+class (IFunctor m) => IMonad m where
+  ireturn :: a ~> m a
+  ibind :: (a ~> m b) -> m a ~> m b
+
+data At :: Type -> k -> k -> Type where
+  At :: a -> At a k k
+
+(>>=) :: (IMonad (m :: (x -> Type) -> x -> Type)) => m a ix -> (a ~> m b) -> m b ix
+m >>= f = ibind f m
+
+returnAt :: (IMonad m) => a -> m (At a k) k
+returnAt = ireturn . At
+
+------------------------Protocol type class-----------------------
+class Protocol role' ps where
+  type Done (sr :: role') :: ps
+  data Msg role' ps (fromSt :: ps) (sender :: role') (senderNewSt :: ps) (receiver :: role') (receiverNewSt :: ps)
+
+--------------------------Action peer -----------------------------
+data Peer role' ps (r :: role') (m :: Type -> Type) (ia :: ps -> Type) (st :: ps) where
+  IReturn :: ia st -> Peer role' ps r m ia st
+  LiftM :: m (Peer role' ps r m ia st') -> Peer role' ps r m ia st
+  Yield
+    :: Msg role' ps from send sps recv rps
+    -> Peer role' ps send m ia sps
+    -> Peer role' ps send m ia from
+  Await
+    :: (Msg role' ps from send sps recv ~> Peer role' ps recv m ia)
+    -> Peer role' ps recv m ia from
+
+------------------------IFunctor and IMonad instance for Peer--------------------------
+instance (Functor m) => IFunctor (Peer role' ps r m) where
+  imap f = \case
+    IReturn ia -> IReturn (f ia)
+    LiftM f' -> LiftM (fmap (imap f) f')
+    Yield ms cont -> Yield ms (imap f cont)
+    Await cont -> Await (imap f . cont)
+
+instance (Functor m) => IMonad (Peer role' ps r m) where
+  ireturn = IReturn
+  ibind f = \case
+    IReturn ia -> (f ia)
+    LiftM f' -> LiftM (fmap (ibind f) f')
+    Yield ms cont -> Yield ms (ibind f cont)
+    Await cont -> Await (ibind f . cont)
+
+-------------------- await function-------------------------
+await
+  :: (Functor m)
+  => Peer role' ps recv m (Msg role' ps from send sps recv) from
+await = Await ireturn
+
+----------------ExampleRole, Example -------------------
+data ExampleRole = Client | Server
   deriving (Show, Eq, Ord, Enum, Bounded)
 
-data PingPongBranchSt = PingPongBranchSt
-  deriving (Show, Eq, Ord, Enum, Bounded)
+data Example = End | S0 | S1
 
-data SPingPongRole (a_a2Vf :: PingPongRole) where
-  SClient :: SPingPongRole 'Client
-  SServer :: SPingPongRole 'Server
-
-type instance Data.IFunctor.Sing = SPingPongRole
-
-instance Data.IFunctor.SingI 'Client where
-  sing = SClient
-instance Data.IFunctor.SingI 'Server where
-  sing = SServer
-instance SingToInt PingPongRole where
-  singToInt x_a2Vg =
-    I# (dataToTag# x_a2Vg)
-
-data PingPong = End | S0 | S1
-
-data SPingPong (a_a2Vi :: PingPong) where
-  SEnd :: SPingPong 'End
-  SS0 :: SPingPong 'S0
-  SS1 :: SPingPong 'S1
-type instance Data.IFunctor.Sing = SPingPong
-
-type ClientStartSt = 'S0
-type ServerStartSt = 'S0
-
-instance Data.IFunctor.SingI 'End where
-  sing = SEnd
-instance Data.IFunctor.SingI 'S0 where
-  sing = SS0
-instance Data.IFunctor.SingI 'S1 where
-  sing = SS1
-instance SingToInt PingPong where
-  singToInt x_a2Vl =
-    I# (dataToTag# x_a2Vl)
-
-instance Protocol PingPongRole PingPong where
+---------------- Protocol instance for ExampleRole and Example-------------------
+instance Protocol ExampleRole Example where
   type Done 'Client = 'End
   type Done 'Server = 'End
   data
     Msg
-      PingPongRole
-      PingPong
-      (from :: PingPong)
-      (send :: PingPongRole)
-      (sendNewSt :: PingPong)
-      (recv :: PingPongRole)
-      (receiverNewSt :: PingPong)
+      ExampleRole
+      Example
+      (from :: Example)
+      (send :: ExampleRole)
+      (sendNewSt :: Example)
+      (recv :: ExampleRole)
+      (receiverNewSt :: Example)
     where
-    Ping :: Msg PingPongRole PingPong 'S0 'Client 'S1 'Server 'S1
-    Ping1 :: Msg PingPongRole PingPong 'S1 'Client 'End 'Server 'End
+    Ping :: Msg ExampleRole Example 'S0 'Client 'S1 'Server 'S1
+    Ping1 :: Msg ExampleRole Example 'S1 'Client 'End 'Server 'End
 
-serverPeer :: Peer PingPongRole PingPong Server IO (At () (Done Server)) S0
-serverPeer = I.do
-  await I.>>= \case
-    Ping -> I.do
-      await I.>>= \case
+----------------------- serverPeer -------------------------
+{-
+Msg Ping,  Client ->  Server
+Msg Ping1, Client -> Server
+-}
+serverPeer :: Peer ExampleRole Example Server IO (At () (Done Server)) S0
+serverPeer = Protocol.do
+  await Protocol.>>= \case
+    Ping -> Protocol.do
+      await Protocol.>>= \case
         Ping1 -> returnAt ()
